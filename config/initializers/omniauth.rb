411e283267bc5705a7ae610cbf1ebe3e8bb6af27
cola-zero <colazero@mail.mkoga.net>
2012-03-23 21:14:45 +0900

    Reverting "add ssl config for heroku." Revert "add ssl config for heroku."
    This reverts commit 70495bde83ea30f7d9a001e3b3ca2906ba844da3.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production? or Rails.env.staging?
-    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY'],
-    {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
-  else
-    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY']
-  end
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY']
 end
