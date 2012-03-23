30ee719ddc544db030fbf329caef8fcac18626d9
cola-zero <colazero@mail.mkoga.net>
2012-03-23 19:45:52 +0900

    use right basic password for staging env.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def password_protected
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['STAGING_BASIC_AUTH_USERNAME'] &&
-        password == ENV['STAGING_BASIC_AUTH_USERNAME']
+        password == ENV['STAGING_BASIC_AUTH_PASSWORD']
     end
   end
 end
