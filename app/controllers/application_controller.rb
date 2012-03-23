e72fa83db287762bab25f3d6ecd467fb6369f19a
cola-zero <colazero@mail.mkoga.net>
2012-03-23 17:40:34 +0900

    enable ssl in production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
     redirect_to "/auth/twitter" unless signed_in?
   end
 
+  before_filter :password_protected if Rails.env.stagins?
+
+  private
+  def password_protected
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['STAGING_BASIC_AUTH_USERNAME'] &&
+        password == ENV['STAGING_BASIC_AUTH_USERNAME']
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Podcastfarm
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.force_ssl = false
   end
 end
